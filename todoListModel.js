46b253eced0773ae0444160e892bd048c5c389da
Akshay Tekam <akshaytekam@atlassian.com>
2022-02-17 19:59:49 +0530

    first commit

---
+++ todoListModel.js
@@ -0,0 +1,27 @@
+// contactModel.js
+var mongoose = require('mongoose');
+// Setup schema
+var todoschema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        required: true,
+        default: "Active"
+    },
+    create_date: {
+        type: Date,
+        default: Date.now
+    }
+});
+// Export Contact model
+var todoList = module.exports = mongoose.model('todoList', todoschema);
+module.exports.get = function (callback, limit) {
+    todoList.find(callback).limit(limit);
+}
